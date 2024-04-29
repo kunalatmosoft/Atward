b23263ab0af226339fa6a44e78369df57f880968
kunalatmosoft <147726001+kunalatmosoft@users.noreply.github.com>
2024-04-29 18:54:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -124,7 +124,7 @@ const LoginPage = () => {
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="#" className="underline">
               Sign up
             </Link>
